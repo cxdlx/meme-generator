834b16626c0f8ed5088390af0ac40be3fe87e5b1
cxdlx <loonyv@outlook.com>
2023-01-10 00:40:05 +0100

    Data

--- src/components/Meme/Meme.js
+++ src/components/Meme/Meme.js
@@ -1,15 +1,24 @@
 import classes from './Meme.module.css';
+import memesData from '../../memesData';
 
 function Meme() {
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        console.log(url)
+    }
+    
     return (
         <div className={classes.container}>
-            <form>
+            <div className={classes.memes__form}>
                 <div className={classes.input__container}>
                     <input type='text' placeholder='Shut up'/>
                     <input type='text' placeholder='and take my money'/>
                 </div>
-                <button type='submit'>Generate</button>
-            </form>
+                <button onClick={getMemeImage}>Generate</button>
+            </div>
         </div>
     )
 }
